542decced9d5b01652a25d68fb21e460eb08acc0
Rachit Singhal <rachitsinghal106@gmail.com>
2026-08-01 13:39:04 +0530

    chore: initialize Spring Boot project with PostgreSQL

---
+++ src/test/java/com/ledgerpay/ledgerpay/LedgerpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ledgerpay.ledgerpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedgerpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
